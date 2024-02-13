8c1086e979459a1c520e9653bc39e3376e4dc992
MagaMariott <101788722+MagaMariott@users.noreply.github.com>
2024-02-13 14:08:33 +0600

    Pusk

--- lab1/syntax.py
+++ lab1/syntax.py
@@ -1,1 +1,6 @@
+#1 Insert the missing part of the code below to output "Hello World".
+print("Hello World")
 
+#2 Complete the code block, print "YES" if 5 is larger than 2.
+if 5 > 2:
+    print("YES")

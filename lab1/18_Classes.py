5eeeb5db86851c6b3de6ee1c50ba1e0dc8b5d0f2
MagaMariott <101788722+MagaMariott@users.noreply.github.com>
2024-02-16 13:32:35 +0600

    Add files via upload

---
+++ lab1/18_Classes.py
@@ -0,0 +1,20 @@
+#1 Create a class named MyClass:
+class MyClass:
+    x = 5
+
+#2 Create an object of MyClass called p1:
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+#3 Use the p1 object to print the value of x:
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+
+#4 What is the correct syntax to assign a "init" function to a class?
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
